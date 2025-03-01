a5e52b34c1f4804cc79725fb94bf9553b574709f
mosiurrahman8383 <mosiurmahfuz357@gmail.com>
2025-03-01 18:10:41 +0600

    added search option funtion

--- js/test.js
+++ js/test.js
@@ -24,7 +24,7 @@ function getTimeString(time){
     return `${hour} hour ${minute} minute ${remainingSecond} second ago`
 }
 
-console.log(getTimeString(3670))
+// console.log(getTimeString(3670));
 
 function secondsToHMS(seconds) {
     const hours = parseInt(seconds / 3600);  // Using parseInt
@@ -33,7 +33,7 @@ function secondsToHMS(seconds) {
   
     return `${hours} hours, ${minutes} minutes, ${remainingSeconds} seconds`;
   }
-  console.log(secondsToHMS(3670))
+  // console.log(secondsToHMS(3670));
 
   function setTime(second){
     const hour = parseInt(second / 3600);
@@ -42,4 +42,17 @@ function secondsToHMS(seconds) {
     return `${hour} hours, ${minute} minutes, ${seconds} seconds ago`
 }
 
-console.log(setTime(3670))
+// console.log(setTime(3670));
+
+
+const users = [
+  { name: "Charlie", age: 30 },
+  { name: "Carlie", age: 30 },
+  { name: "Alice", age: 25 },
+  { name: "Bob", age: 20 }
+];
+
+// Sort by name (A to Z)
+users.sort((a, b) => a.name.localeCompare(b.name));
+
+console.log(users);
--- js/video.js
+++ js/video.js
@@ -25,8 +25,8 @@ const loadCategories = () => {
         .catch(error => console.log(error))
 }
 
-const loadVideos = () => {
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+const loadVideos = (searchText  = "") => {
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
         .then(res => res.json())
         .then(data => displayVideos(data.videos))
         .catch(error => console.log(error))
@@ -189,7 +189,11 @@ const  displayCategories = (categories) => {
 
 
     });
-}
+};
+
+document.getElementById('search-input').addEventListener('keyup',(e) => {
+    loadVideos(e.target.value)
+})
 
 loadCategories();
 loadVideos();
